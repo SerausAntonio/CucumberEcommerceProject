f6c5f2f90ce41c265162155adba3e68c247ce74c
Antonio Seraus <aseraus@hotmail.com>
2022-04-10 19:46:07 +0200

    stepDefinitions added

--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -1,4 +1,38 @@
 package stepDefinitions;
 
+import io.cucumber.java.af.Dan;
+import io.cucumber.java.nl.Als;
+import io.cucumber.java.nl.Gegeven;
+
 public class StepDefinition {
+
+
+    @Gegeven("ik ben op de log in pagina")
+    public void ik_ben_op_de_log_in_pagina() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Als("ik mijn gebruikersnaam intik")
+    public void ik_mijn_gebruikersnaam_intik() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Als("Mijn wachtwoord intik")
+    public void mijn_wachtwoord_intik() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Als("ik klik op Inloggen")
+    public void ik_klik_op_inloggen() {
+       // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Dan("ben ik op de welkom pagina")
+    public void ben_ik_op_de_welkom_pagina() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
 }
