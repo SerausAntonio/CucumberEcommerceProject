d3eeb5da0788ed38a3ee8594aea176d858ae882e
Antonio Seraus <aseraus@hotmail.com>
2022-04-10 19:59:53 +0200

    runner added

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,4 +1,11 @@
 package runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( features="src//features//Login.feature",
+        glue = "stepDefinitions")
 public class Runner {
 }
