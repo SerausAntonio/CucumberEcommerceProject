0843059b01c45ad82612473e4a38455b302a354d
Antonio Seraus <aseraus@hotmail.com>
2022-04-17 21:46:18 +0200

    stepdefinitions added

--- src/features/login.feature
+++ src/features/login.feature
@@ -5,7 +5,7 @@
     Scenario: Log in met de juiste credentialen
 
     Gegeven ik ben op de log in pagina
-    Als ik mijn gebruikersnaam intik
-    En Mijn wachtwoord intik
+    Als ik mijn gebruikersnaam "user" invul
+    En ik mijn wachtwoord "pass" invul
     En ik klik op Inloggen
     Dan ben ik op de welkom pagina
--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -6,32 +6,28 @@ import io.cucumber.java.nl.Gegeven;
 
 public class StepDefinition {
 
-
     @Gegeven("ik ben op de log in pagina")
     public void ik_ben_op_de_log_in_pagina() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("ik ben ingelogd");
     }
-    @Als("ik mijn gebruikersnaam intik")
-    public void ik_mijn_gebruikersnaam_intik() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Als("Mijn wachtwoord intik")
-    public void mijn_wachtwoord_intik() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    @Als("ik mijn gebruikersnaam {string} invul")
+    public void ik_mijn_gebruikersnaam_invul(String string) {
+        System.out.println("ik vul mijn gebruikersnaam " + string);
+     }
+    @Als("ik mijn wachtwoord {string} invul")
+    public void mijn_wachtwoord_invul(String string) {
+        System.out.println("Mijn wachtwoord invult " + string);
 
+    }
     @Als("ik klik op Inloggen")
     public void ik_klik_op_inloggen() {
-       // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Ik klik op inloggen");
+
     }
     @Dan("ben ik op de welkom pagina")
     public void ben_ik_op_de_welkom_pagina() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Ik ben op de welkom pagina");
+
     }
 
 
